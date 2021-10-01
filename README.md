e12cd611f2af95d0d911a6da5701023cf4322523
Yuri Matos <yuri.cabello@hotmail.com>
2021-10-01 01:13:20 -0300

    ajustes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <h1>Projeto simples em TypeScript</h1>
 </br>
 <p>NavBar responsivo no ReactJS, para facilitar os projetos responsivos</p>
+<p>Projetei esse navbar afim de ajudar na hora da criação de seu site...</p>
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes/Index';
-import Navbar from './Components/Navbar';
+import Header from './Components/Header';
 import GlobalStyles from './Styles/global';
 
 const App = () => (
     <>
-      <Navbar />
+      <Header />
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
